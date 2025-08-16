a99e48afa61ab44a5f2681d1862e28c69d6c04ce
Talha Qureshi <talha_qureshi@hotmail.com>
2025-08-16 21:35:03 +0500

    useCookies hook added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Common-Custom-React-Hooks
-Helper repo with some react hooks we use commonly on projects
+Helper repo with some react hooks we use commonly on projects, more hooks will be added as we fine tune them and have the time to work on this repo
---
+++ Typescript/useCookies.ts
@@ -0,0 +1,56 @@
+import { useCallback } from 'react';
+
+export function useCookies() {
+  const setCookie = useCallback((
+    cookieName: string,
+    value: string,
+    expiryInMinutes: number,
+    path?: string,
+    domain?: string
+  ) => {
+    let expires = "";
+    if (expiryInMinutes) {
+      const date = new Date();
+      date.setTime(date.getTime() + expiryInMinutes * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    const pathStr = `; path=${path || '/'}`;
+    // Optional: add a specific domain to the cookie, useful for when sub-domains exist
+    const domainStr = domain ? `; domain=${domain}` : "";
+    // Optional: Secure cookie (only sent over HTTPS)
+    const secureStr = typeof window !== 'undefined' && window.location.protocol === "https:" ? "; secure" : "";
+
+    // This is done to prevent errors during server-side rendering
+    if (typeof document !== 'undefined') {
+      document.cookie = `${cookieName}=${value}${expires}${pathStr}${domainStr}${secureStr}`;
+    }
+  }, []);
+
+  const getCookie = useCallback((cookieName: string): string | undefined => {
+    if (typeof document === 'undefined') return undefined;
+
+    const _cookieName = cookieName + "=";
+    const ca = document.cookie.split(';');
+
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(_cookieName) === 0) {
+        return c.substring(_cookieName.length, c.length);
+      }
+    }
+    return undefined;
+  }, []);
+
+  const removeCookie = useCallback((cookieName: string, path?: string, domain?: string) => {
+    setCookie(cookieName, "", -1, path, domain);
+  }, [setCookie]);
+
+  const cookieExists = useCallback((cookieName: string): boolean => {
+    return typeof getCookie(cookieName) !== "undefined";
+  }, [getCookie]);
+
+  return { setCookie, getCookie, removeCookie, cookieExists };
+}
